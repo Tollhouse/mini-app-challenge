b8dfdc1c1a7b6060a3782d0a93aa3bfc36733274
Tollhouse <mistertollhouse@gmail.com>
2025-03-27 08:54:58 -0600

    Level 0 complete had to look up map function for making a list of elements

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+  ];
+
+  return (
+    <>
+      <div class="movieList">
+        {movies.map(movie => <p>{movie.title}</p>)}
+      </div>
+    </>
+  )
+}
+
+export default App
